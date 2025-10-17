bc2a6c341d6dfb83dfa2a7fe85fdd7733751613d
David Chilito <jdchilito@unicauca.edu.co>
2025-10-17 17:44:17 -0500

    Se quito la url de los controllers

--- src/main/java/co/unicauca/edu/microservicio/microservicio_usuarios/infrastructure/input/controllers/UserController.java
+++ src/main/java/co/unicauca/edu/microservicio/microservicio_usuarios/infrastructure/input/controllers/UserController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping()
 @RequiredArgsConstructor
 public class UserController {
     private final UserManagementIntPort userManagementUseCaseIntPort;
